3cd58fc2c3a0c1ef4e1f9939beea35f86e22a84f
Meenakshi Parameshwaran <Meenakshi.Parameshwaran@Rightmove.co.uk>
2019-03-20 21:27:09 +0000

    Add numpydoc extension to conf.py

--- conf.py
+++ conf.py
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
